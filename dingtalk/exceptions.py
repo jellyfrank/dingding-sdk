75ed2ab611196f98f28f0f7a765b35ff8efd72db
block-cat <kfx2007@outlook.com>
2021-06-26 05:45:39 +0000

    [ADD]exception

--- dingtalk/exceptions.py
+++ dingtalk/exceptions.py
@@ -13,4 +13,4 @@ class DingTalkException(Exception):
         self.request_id = request_id
 
     def __str__(self):
-        return f"errcode: {self.errcode}, errmsg:{self.errmsg}, request_id:{self.request_id} submsg:{self.sub_msg or ''}"
+        return f"errcode: {self.errcode},  errmsg: {self.errmsg}, request_id: {self.request_id} {f'submsg:{self.sub_msg}' if self.sub_msg else '' }" 
